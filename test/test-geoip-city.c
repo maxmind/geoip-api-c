e14c2d9cc38bb492262cf89a34d4cb80a4bb675d
Thomas J Mather <geoipapi@maxmind.com>
2003-04-09 21:54:02 +0000

    added dma and area code

--- test/test-geoip-city.c
+++ test/test-geoip-city.c
@@ -50,13 +50,15 @@ int main (int argc, char* argv[]) {
 		gir = GeoIP_record_by_name (gi, (const char *)host);
 
 		if (gir != NULL) {
-			printf("%s\t%s\t%s\t%s\t%s\t%f\t%f\n", host,
+			printf("%s\t%s\t%s\t%s\t%s\t%f\t%f\t%d\t%d\n", host,
 					 gir->country_code,
 					 gir->region,
 					 gir->city,
 					 gir->postal_code,
 					 gir->latitude,
-					 gir->longitude);
+					 gir->longitude,
+					 gir->dma_code,
+					 gir->area_code);
 			GeoIPRecord_delete(gir);
 		}
 	}
