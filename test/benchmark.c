19aa3f4f162873cb0a7e23698206da9c25b0cf9f
Thomas J Mather <geoipapi@maxmind.com>
2007-01-26 19:45:06 +0000

    Moved var definitions to top of code (Frank)

--- test/benchmark.c
+++ test/benchmark.c
@@ -11,78 +11,87 @@ struct timeval timer_t1;
 struct timeval timer_t2;
 
 
-void timerstart(){
+void timerstart() {
   gettimeofday(&timer_t1,NULL);
 }
-double timerstop(){
+double timerstop() {
+  int a1 = 0;
+  int a2 = 0;
+  double r = 0;
   gettimeofday(&timer_t2,NULL);
-  int a1 = timer_t2.tv_sec - timer_t1.tv_sec;
-  int a2 = timer_t2.tv_usec - timer_t1.tv_usec;
-  if (a1 < 0){
+  a1 = timer_t2.tv_sec - timer_t1.tv_sec;
+  a2 = timer_t2.tv_usec - timer_t1.tv_usec;
+  if (a1 < 0) {
     a1 = a1 - 1;
     a2 = a2 + 1000000;
   }
-  double r = (((double) a1) + (((double) a2) / 1000000));
+  r = (((double) a1) + (((double) a2) / 1000000));
   return r;
 }
 
-void testgeoipcountry(int flags,const char *msg,int numlookups){
-  GeoIP *i = GeoIP_open("/usr/local/share/GeoIP/GeoIP.dat",flags);
-  if (i == NULL){
+void testgeoipcountry(int flags,const char *msg,int numlookups) {
+  const char *str = NULL;
+  double t = 0;
+  int i4 = 0;
+  int i2 = 0;
+  GeoIP *i = NULL;
+  i = GeoIP_open("/usr/local/share/GeoIP/GeoIP.dat",flags);
+  if (i == NULL) {
     printf("error: GeoIP.dat does not exist\n");
     return;
   }
-  const char *str;
   timerstart();
-  int i4 = 0;
-  int i2 = 0;
-  for (i2 = 0;i2 < numlookups;i2++){
+  for (i2 = 0;i2 < numlookups;i2++) {
     str = GeoIP_country_name_by_addr(i,ipstring[i4]);
     i4 = (i4 + 1) % numipstrings;
   }
-  double t = timerstop();
+  t = timerstop();
   printf("%s\n", msg);
   printf("%d lookups made in %f seconds \n",numlookups,t);
   GeoIP_delete(i);
 }
 
-void testgeoipregion(int flags,const char *msg,int numlookups){
-  GeoIP *i = GeoIP_open("/usr/local/share/GeoIP/GeoIPRegion.dat",flags);
-  if (i == NULL){
+void testgeoipregion(int flags,const char *msg,int numlookups) {
+  GeoIP *i = NULL;
+  GeoIPRegion *i3 = NULL;
+  int i4 = 0;
+  int i2 = 0;
+  double t = 0;
+  i = GeoIP_open("/usr/local/share/GeoIP/GeoIPRegion.dat",flags);
+  if (i == NULL) {
     printf("error: GeoIPRegion.dat does not exist\n");
     return;
   }
-  GeoIPRegion *i3;
   timerstart();
-  int i4 = 0;
-  int i2 = 0;
-  for (i2 = 0;i2 < numlookups;i2++){
+  for (i2 = 0;i2 < numlookups;i2++) {
     i3 = GeoIP_region_by_addr(i,ipstring[i4]);
     GeoIPRegion_delete(i3); 
     i4 = (i4 + 1) % numipstrings;
   }
-  double t = timerstop();
+  t = timerstop();
   printf("%s\n", msg);
   printf("%d lookups made in %f seconds \n",numlookups,t);
   GeoIP_delete(i);
 }
 
-void testgeoipcity(int flags,const char *msg,int numlookups){
-  GeoIP *i = GeoIP_open("/usr/local/share/GeoIP/GeoIPCity.dat",flags);
-  if (i == NULL){
+void testgeoipcity(int flags,const char *msg,int numlookups) {
+  GeoIP *i = NULL;
+  GeoIPRecord * i3 = NULL;
+  int i4 = 0;
+  int i2 = 0;
+  double t = 0;
+  i = GeoIP_open("/usr/local/share/GeoIP/GeoIPCity.dat",flags);
+  if (i == NULL) {
     printf("error: GeoLiteCity.dat does not exist\n");
     return;
   }
-  GeoIPRecord * i3;
   timerstart();
-  int i4 = 0;
-  int i2 = 0;
-  for (i2 = 0;i2 < numlookups;i2++){
+  for (i2 = 0;i2 < numlookups;i2++) {
     i3 = GeoIP_record_by_addr(i,ipstring[i4]);
     GeoIPRecord_delete(i3);
     i4 = (i4 + 1) % numipstrings;
   }
-  double t = timerstop();
+  t = timerstop();
   printf("%s\n", msg);
   printf("%d lookups made in %f seconds \n",numlookups,t);
   GeoIP_delete(i);
