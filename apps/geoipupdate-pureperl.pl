07397e5c22597f281c7a908b5fc2d733183ecc1a
Boris Zentner <bzentner@maxmind.com>
2012-12-05 18:26:29 +0100

    Indent geoipupdate-pureperl.pl

--- apps/geoipupdate-pureperl.pl
+++ apps/geoipupdate-pureperl.pl
@@ -64,197 +64,206 @@ my $proto        = 'http';
 my %opts;
 
 if ( !getopts( 'hvf:d:', \%opts ) or $opts{h} ) {
-  print STDERR
-    "Usage: geoipupdate [-hv] [-f license_file] [-d custom directory]\n";
-  exit @ARGV ? 1 : 0;
+    print STDERR
+        "Usage: geoipupdate [-hv] [-f license_file] [-d custom directory]\n";
+    exit @ARGV ? 1 : 0;
 }
 
 my $rootdir = File::Spec->rootdir;
 $opts{d} ||= File::Spec->catfile( $rootdir, qw/ usr local share GeoIP / );
-$opts{f} ||=
-  File::Spec->catfile( $rootdir, qw/ usr local etc /, $license_file );
+$opts{f}
+    ||= File::Spec->catfile( $rootdir, qw/ usr local etc /, $license_file );
 
 die "dir $opts{d} does not exist or is not readable or is not a directory\n"
-  unless -d $opts{d};
+    unless -d $opts{d};
 die "license_file $opts{f} does not exist, is not readable or is not a file\n"
-  unless -f $opts{f};
+    unless -f $opts{f};
 
 #
 # --- parse license file
 #
 open my $fh, '<', $opts{f}
-  or die "Error opening GeoIP Configuration file $opts{f}\n";
+    or die "Error opening GeoIP Configuration file $opts{f}\n";
 print "Opened License file $opts{f}\n" if $opts{v};
 
 my ( $user_id, $license_key, @product_ids );
 {
-  local $_;
+    local $_;
 
-  while (<$fh>) {
-    next if /^\s*#/;    # skip comments
-    /^\s*UserId\s+(\d+)/        and $user_id     = $1, next;
-    /^\s*LicenseKey\s+(\S{12})/ and $license_key = $1, next;
-    /^\s*ProductIds\s+(\d+(?:[a-zA-Z]{2,3})?(?:\s+\d+(?:[a-zA-Z]{2,3})?)*)/    
-      and @product_ids = split( /\s+/, $1 ), next;
+    while (<$fh>) {
+        next if /^\s*#/;    # skip comments
+        /^\s*UserId\s+(\d+)/        and $user_id     = $1, next;
+        /^\s*LicenseKey\s+(\S{12})/ and $license_key = $1, next;
+        /^\s*ProductIds\s+(\d+(?:[a-zA-Z]{2,3})?(?:\s+\d+(?:[a-zA-Z]{2,3})?)*)/
+            and @product_ids = split( /\s+/, $1 ), next;
 
-  }
+    }
 }
 
 if ( $opts{v} ) {
-  print "User id $user_id\n" if $user_id;
-  print "Read in license key $license_key\n";
-  print "Product ids @product_ids\n";
+    print "User id $user_id\n" if $user_id;
+    print "Read in license key $license_key\n";
+    print "Product ids @product_ids\n";
 }
 
 if ($user_id) {
-  for my $product_id (@product_ids) {
+    for my $product_id (@product_ids) {
 
-    # update the databases using the user id string,
-    # the license key string and the product id for each database
-    eval {
-      GeoIP_update_database_general( $user_id,    $license_key,
-                                     $product_id, $opts{v} );
-    };
+        # update the databases using the user id string,
+        # the license key string and the product id for each database
+        eval {
+            GeoIP_update_database_general(
+                $user_id,    $license_key,
+                $product_id, $opts{v}
+            );
+        };
+        my $err = $@;
+        die $err if $err and $err !~ /^No new updates available/i;
+        print $err;
+    }
+}
+else {
+
+    # Old format with just license key for MaxMind GeoIP Country database updates
+    # here for backwards compatibility
+    eval { GeoIP_update_database( $license_key, $opts{v} ); };
     my $err = $@;
     die $err if $err and $err !~ /^No new updates available/i;
     print $err;
-  }
-} else {
-
-  # Old format with just license key for MaxMind GeoIP Country database updates
-  # here for backwards compatibility
-  eval { GeoIP_update_database( $license_key, $opts{v} ); };
-  my $err = $@;
-  die $err if $err and $err !~ /^No new updates available/i;
-  print $err;
 }
 
 exit 0;
 
 sub GeoIP_update_database_general {
-  my ( $user_id, $license_key, $product_id, $verbose, $client_ipaddr ) = @_;
-  my $u = URI->new("$proto://$update_host/app/update_getfilename");
-  $u->query_form( product_id => $product_id );
+    my ( $user_id, $license_key, $product_id, $verbose, $client_ipaddr ) = @_;
+    my $u = URI->new("$proto://$update_host/app/update_getfilename");
+    $u->query_form( product_id => $product_id );
 
-  print 'Send request ' . $u->as_string, "\n" if ($verbose);
-  my $res = $ua->request( GET $u->as_string, Host => $update_host );
-  die $res->status_line unless ( $res->is_success );
-  # make sure to use only the filename for security reason
-  my $geoip_filename = File::Spec->catfile( $opts{d}, basename($res->content) );
-
-  # /* get MD5 of current GeoIP database file */
-  my $old_md5 = _get_hexdigest($geoip_filename);
-
-  print "MD5 sum of database $geoip_filename is $old_md5\n" if $verbose;
-
-  unless ($client_ipaddr) {
     print 'Send request ' . $u->as_string, "\n" if ($verbose);
-
-    # /* get client ip address from MaxMind web page */
-    $res = $ua->request( GET "$proto://$update_host/app/update_getipaddr",
-                         Host => $update_host );
+    my $res = $ua->request( GET $u->as_string, Host => $update_host );
     die $res->status_line unless ( $res->is_success );
-    $client_ipaddr = $res->content;
-  }
 
-  print "client ip address: $client_ipaddr\n" if $verbose;
-  my $hex_digest2 =
-    Digest::MD5->new->add( $license_key, $client_ipaddr )->hexdigest;
-  print "md5sum of ip address and license key is $hex_digest2\n" if $verbose;
+    # make sure to use only the filename for security reason
+    my $geoip_filename
+        = File::Spec->catfile( $opts{d}, basename( $res->content ) );
 
-  my $mk_db_req_cref = sub {
+    # /* get MD5 of current GeoIP database file */
+    my $old_md5 = _get_hexdigest($geoip_filename);
 
-    $u->path('/app/update_secure');
-    $u->query_form(
-                    db_md5        => shift,
-                    challenge_md5 => $hex_digest2,
-                    user_id       => $user_id,
-                    edition_id    => $product_id
-    );
-    print 'Send request ' . $u->as_string, "\n" if ($verbose);
-    return $ua->request( GET $u->as_string, Host => $update_host );
-  };
-  $res = $mk_db_req_cref->($old_md5);
-  die $res->status_line unless ( $res->is_success );
+    print "MD5 sum of database $geoip_filename is $old_md5\n" if $verbose;
 
-  # print Dumper($res);
-  print "Downloading gzipped GeoIP Database...\n" if $verbose;
+    unless ($client_ipaddr) {
+        print 'Send request ' . $u->as_string, "\n" if ($verbose);
 
-  _gunzip_and_replace(
-    $res->content,
-    $geoip_filename,
-    sub {
-
-      # as sanity check request a update for the new downloaded file
-      # md5 of the new unpacked file
-      my $new_md5 = _get_hexdigest(shift);
-      return $mk_db_req_cref->($new_md5);
+        # /* get client ip address from MaxMind web page */
+        $res = $ua->request(
+            GET "$proto://$update_host/app/update_getipaddr",
+            Host => $update_host
+        );
+        die $res->status_line unless ( $res->is_success );
+        $client_ipaddr = $res->content;
     }
-  );
-  print "Done\n" if $verbose;
+
+    print "client ip address: $client_ipaddr\n" if $verbose;
+    my $hex_digest2
+        = Digest::MD5->new->add( $license_key, $client_ipaddr )->hexdigest;
+    print "md5sum of ip address and license key is $hex_digest2\n"
+        if $verbose;
+
+    my $mk_db_req_cref = sub {
+
+        $u->path('/app/update_secure');
+        $u->query_form(
+            db_md5        => shift,
+            challenge_md5 => $hex_digest2,
+            user_id       => $user_id,
+            edition_id    => $product_id
+        );
+        print 'Send request ' . $u->as_string, "\n" if ($verbose);
+        return $ua->request( GET $u->as_string, Host => $update_host );
+    };
+    $res = $mk_db_req_cref->($old_md5);
+    die $res->status_line unless ( $res->is_success );
+
+    # print Dumper($res);
+    print "Downloading gzipped GeoIP Database...\n" if $verbose;
+
+    _gunzip_and_replace(
+        $res->content,
+        $geoip_filename,
+        sub {
+
+            # as sanity check request a update for the new downloaded file
+            # md5 of the new unpacked file
+            my $new_md5 = _get_hexdigest(shift);
+            return $mk_db_req_cref->($new_md5);
+        }
+    );
+    print "Done\n" if $verbose;
 }
 
 sub GeoIP_update_database {
-  my ( $license_key, $verbose ) = @_;
-  my $geoip_filename = File::Spec->catfile( $opts{d}, 'GeoIP.dat' );
+    my ( $license_key, $verbose ) = @_;
+    my $geoip_filename = File::Spec->catfile( $opts{d}, 'GeoIP.dat' );
 
-  # /* get MD5 of current GeoIP database file */
-  my $hexdigest = _get_hexdigest($geoip_filename);
+    # /* get MD5 of current GeoIP database file */
+    my $hexdigest = _get_hexdigest($geoip_filename);
 
-  print "MD5 sum of database $geoip_filename is $hexdigest\n" if $verbose;
+    print "MD5 sum of database $geoip_filename is $hexdigest\n" if $verbose;
 
-  my $u = URI->new("$proto://$update_host/app/update");
-  $u->query_form( license_key => $license_key, md5 => $hexdigest );
+    my $u = URI->new("$proto://$update_host/app/update");
+    $u->query_form( license_key => $license_key, md5 => $hexdigest );
 
-  print 'Send request ' . $u->as_string, "\n" if ($verbose);
-  my $res = $ua->request( GET $u->as_string, Host => $update_host );
-  die $res->status_line unless ( $res->is_success );
-  print "Downloading gzipped GeoIP Database...\n" if $verbose;
-  _gunzip_and_replace( $res->content, $geoip_filename );
-  print "Done\n" if $verbose;
+    print 'Send request ' . $u->as_string, "\n" if ($verbose);
+    my $res = $ua->request( GET $u->as_string, Host => $update_host );
+    die $res->status_line unless ( $res->is_success );
+    print "Downloading gzipped GeoIP Database...\n" if $verbose;
+    _gunzip_and_replace( $res->content, $geoip_filename );
+    print "Done\n" if $verbose;
 
 }
 
 # --- hexdigest of the file or 00000000000000000000000000000000
 sub _get_hexdigest {
-  my $md5 = '0' x 32;
-  if ( open my $fh, '<:raw', shift ) {
-    $md5 = Digest::MD5->new->addfile($fh)->hexdigest;
-  }
-  return $md5;
+    my $md5 = '0' x 32;
+    if ( open my $fh, '<:raw', shift ) {
+        $md5 = Digest::MD5->new->addfile($fh)->hexdigest;
+    }
+    return $md5;
 }
 
 sub _gunzip_and_replace {
-  my ( $content, $geoip_filename, $sanity_check_c ) = @_;
-  my $max_retry = 1;
+    my ( $content, $geoip_filename, $sanity_check_c ) = @_;
+    my $max_retry = 1;
 
-  my $tmp_fname = $geoip_filename . '.test';
+    my $tmp_fname = $geoip_filename . '.test';
 
-  {
-
-    # --- error if our content does not start with the gzip header
-    die $content || 'Not a gzip file' if substr( $content, 0, 2 ) ne "\x1f\x8b";
-
-    # --- uncompress the gzip data
     {
-      local $_;
-      open my $gin,  '<:gzip', \$content  or die $!;
-      open my $gout, '>:raw',  $tmp_fname or die $!;
-      print {$gout} $_ while (<$gin>);
+
+        # --- error if our content does not start with the gzip header
+        die $content || 'Not a gzip file'
+            if substr( $content, 0, 2 ) ne "\x1f\x8b";
+
+        # --- uncompress the gzip data
+        {
+            local $_;
+            open my $gin,  '<:gzip', \$content  or die $!;
+            open my $gout, '>:raw',  $tmp_fname or die $!;
+            print {$gout} $_ while (<$gin>);
+        }
+
+        # --- sanity check
+        if ( defined $sanity_check_c ) {
+            die "Download failed" if $max_retry-- <= 0;
+            my $res = $sanity_check_c->($tmp_fname);
+            die $res->status_line unless ( $res->is_success );
+            $content = $res->content;
+
+            redo if ( $content !~ /^No new updates available/ );
+        }
     }
 
-    # --- sanity check
-    if ( defined $sanity_check_c ) {
-      die "Download failed" if $max_retry-- <= 0;
-      my $res = $sanity_check_c->($tmp_fname);
-      die $res->status_line unless ( $res->is_success );
-      $content = $res->content;
-
-      redo if ( $content !~ /^No new updates available/ );
-    }
-  }
-
-  # --- install GeoIP.dat.test -> GeoIP.dat
-  rename( $tmp_fname, $geoip_filename ) or die $!;
+    # --- install GeoIP.dat.test -> GeoIP.dat
+    rename( $tmp_fname, $geoip_filename ) or die $!;
 }
 
