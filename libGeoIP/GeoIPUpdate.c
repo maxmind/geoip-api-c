bc7042b1d3bf909f824111bf6adbaac80a24280e
Boris Zentner <bzentner@maxmind.com>
2013-10-28 15:37:35 +0100

    Accept only 200 OK responses from the server

--- libGeoIP/GeoIPUpdate.c
+++ libGeoIP/GeoIPUpdate.c
@@ -634,6 +634,17 @@ short int GeoIP_update_database_general (char * user_id,char * license_key,char 
 	}
 	buf[offset] = 0;
 	offset = 0;
+
+        tmpstr = strchr(buf, "\n");
+        if ( tmpstr == NULL ) {
+	  free(buf);
+	  return GEOIP_INVALID_SERVER_RESPONSE;
+        }
+        tmpstr = strnstr(buf, " 200 OK", tmpstr - buf);
+        if ( tmpstr == NULL ) {
+          free(buf);
+	  return GEOIP_INVALID_SERVER_RESPONSE;
+        }
 	tmpstr = strstr(buf, "\r\n\r\n");
         if ( tmpstr == NULL ) {
    		free(buf);
