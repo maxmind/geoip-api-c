2d09006c29820305f6724203e69f5585a75f9b71
Gregory Oschwald <goschwald@maxmind.com>
2015-09-03 09:48:36 -0700

    Update region code and timezone code generation
    This is based off of updated scripts from elliotlo in GitHub issue #64.
    
    This should fix a parser stack overflow issue that was reported in
    MSVC.

--- timezone/generate_timeZone.pl
+++ timezone/generate_timeZone.pl
@@ -1,5 +1,9 @@
 #!/usr/bin/perl
 use strict;
+use warnings;
+use autodie;
+
+use Text::CSV_XS ();
 
 # Obtain timezone.txt from http://www.maxmind.com/timezone.txt
 
@@ -8,56 +12,64 @@ use strict;
 
 my $tz;
 
-open my $fh, '<', 'timezone.txt' or die $!;
+open my $fh, '<:encoding(latin1)', 'timezone.txt';
+
+my $csv = Text::CSV_XS->new( { binary => 1, auto_diag => 1 } );
 
 print "#include <string.h> \n";
 print
-"const char* GeoIP_time_zone_by_country_and_region(const char * country,const char * region) {\n";
-print "  const char* timezone = NULL;\n";
-print "  if (country == NULL) {\n";
-print "    return NULL;\n";
-print "  }\n";
-print "  if (region == NULL) {\n";
-print '    region = "";', "\n";
-print "  }\n";
+    "const char* GeoIP_time_zone_by_country_and_region(const char * country,const char * region) {\n";
+print "    const char* timezone = NULL;\n";
+print "    if (country == NULL) {\n";
+print "      return NULL;\n";
+print "    }\n";
+print "    if (region == NULL) {\n";
+print '        region = "";', "\n";
+print "    }\n";
 
-local $_ = <$fh>;    # skip first line
-while (<$fh>) {
-  chomp;
-  my ( $country, $region, $timezone ) = split /\t/;
-  die "$_ $.\n" unless $timezone;
-  $tz->{$country}->{ $region || '' } = $timezone;
+<$fh>;    # skip first line
+while ( my $row = $csv->getline($fh) ) {
+    my ( $country, $region, $timezone ) = @{$row};
+    die "$_ $.\n" unless $timezone;
+    $tz->{$country}->{ $region || q{} } = $timezone;
 }
 
 my $first_country;
+
+$first_country = 0;
 for my $c ( sort keys %$tz ) {
-  print( ( defined $first_country ) ? '  else if' : '  if' );
-  $first_country ||= $c;
-  my $def = delete $tz->{$c}->{''};
-  if ( my @reg = sort keys %{ $tz->{$c} } ) {
-    my @tz = map{ $tz->{$c}->{$_} } @reg;
+    print('    if');
+    $first_country ||= $c;
+    my $def = delete $tz->{$c}->{q{}};
+    if ( my @reg = sort keys %{ $tz->{$c} } ) {
+        my @tz = map { $tz->{$c}->{$_} } @reg;
 
-    printf( qq! ( strcmp (country, "%s") == 0 ) {\n!, $c );
-    for ( 0 .. $#reg ) {
+        printf( qq! ( strcmp (country, "%s") == 0 ) {\n!, $c );
+        for ( 0 .. $#reg ) {
 
-      # have regions
+            # have regions
 
-      print( ( $_ == 0 ) ? '    if' : '    else if' );
-      printf( qq! ( strcmp (region, "%s") == 0 ) {\n!, $reg[$_] );
-      printf( qq!      timezone = "%s";\n!,            $tz[$_] );
-      printf( qq!    }\n! );
+            print( ( $_ == 0 ) ? '        if' : '        else if' );
+            printf( qq! ( strcmp (region, "%s") == 0 ) {\n!, $reg[$_] );
+            printf( qq!            return "%s";\n!,                $tz[$_] );
+            printf(qq!        }\n!);
+        }
+        if ( defined $def ) {
+            printf( qq!    else { return "%s"; }\n!, $def );
+        }
+        else {
+            print "        else {\n             return NULL;\n        }\n";
+        }
+        print qq[    }\n];
     }
-    printf( qq!  else { timezone = "%s"; }\n!, $def ) if defined $def;
-    print qq[  }\n];
-  }
-  else {
+    else {
 
-    # only default tz
-    printf( qq! ( strcmp (country, "%s") == 0 ) {\n!, $c );
-    printf( qq!    timezone = "%s";\n!,               $def );
-    printf(qq!  }\n!);
+        # only default tz
+        printf( qq! ( strcmp (country, "%s") == 0 ) {\n!, $c );
+        printf( qq!        return "%s";\n!,                   $def );
+        printf(qq!    }\n!);
 
-  }
+    }
 }
 
-print qq[  return timezone;\n}\n];
+print qq[    return timezone;\n}\n];
